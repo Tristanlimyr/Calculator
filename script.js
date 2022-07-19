bf26758c6d69857266821173b30dcdb42191ec56
Tristanlimyr <Tristanlimyr@gmail.com>
2022-07-19 17:33:14 +0800

    Improve on calculating function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const buttons = document.querySelectorAll('.button');
+const display = document.querySelector('.display');
+const pressButtonColor = 'rgb(255,217,119)';
+const defaultButtonColor = 'rgb(255, 244, 234)';
+let retainResult = false;
 let stack = [0];
 let buttonPressed;
 
@@ -19,6 +23,10 @@ function divide(a, b) {
     return a / b;
 }
 
+function modulo(a, b) {
+    return a % b;
+}
+
 // operate
 function operate(a, b, operator) {
     switch (operator) {
@@ -30,6 +38,8 @@ function operate(a, b, operator) {
             return multiply(a, b);
         case "/": // division
             return divide(a, b);
+        case "%": // modulo
+            return modulo(a, b);
     }
 }
 
@@ -42,6 +52,19 @@ buttons.forEach((button) => {
     })
 })
 
+buttons.forEach((button) => {
+    button.addEventListener('mousedown', changeColor);
+    button.addEventListener('mouseup', changeBackColor);
+})
+
+function changeColor(e) {
+    e.target.style.backgroundColor = pressButtonColor;
+}
+
+function changeBackColor(e) {
+    e.target.style.backgroundColor = defaultButtonColor;
+}
+
 // stack will contain Number type for numbers 
 // String type for operators
 function addToStack(input) {
@@ -49,7 +72,7 @@ function addToStack(input) {
     let a;
     let b;
     let operator;
-    if (Number(input)) { // input is a number
+    if (Number(input) || Number(input) == 0) { // input is a number
         if (Number(prev) || prev == 0) { // prev is a number
             stack.pop();
             prev = (Number(prev) * 10) + Number(input);
@@ -66,7 +89,8 @@ function addToStack(input) {
                 operator = stack.pop();
                 a = stack.pop();
                 stack = [0]; // refresh stack 
-                return operate(a, b, operator);
+                displayResult(operate(a, b, operator));
+                retainResult = true;
             }
             else if (!Number(prev)) { // have to evaluate but with one number
                 operator = stack.pop();
@@ -74,16 +98,30 @@ function addToStack(input) {
                 addToStack(operate(b, b, operator));
             }
         }
+        else if (input == "AC") {
+            stack = [0];
+            display.textContent = 0;
+        }
         else { // input is an operator
             if (stack.length == 3) { // have to evaluate and update
                 b = stack.pop();
                 operator = stack.pop();
                 a = stack.pop();
-                addToStack(operate(a, b, operator));
+                let result = operate(a, b, operator);
+                displayResult(result);
+                addToStack(result);
                 addToStack(input);
             }
             else { // add into stack
+                if (typeof (prev) == 'string') { // remove previous operator
+                    stack.pop();
+                }
+                if (retainResult) {
+                    retainResult = false;
+                    addToStack(Number(display.textContent));
+                }
                 if (stack.length == 0) { // stack is empty
+                    console.log("hi");
                     stack.push(0);
                 }
                 stack.push(input);
@@ -92,5 +130,7 @@ function addToStack(input) {
     }
 }
 
-
 // display result
+function displayResult(result) {
+    display.textContent = String(result);
+}
